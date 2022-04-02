7ba1be868540861039dd610a1a5c2318099c2e8f
Guillermo Avila Martin <Ghostguille2000@gmail.com>
2022-04-02 02:02:23 +0200

    Se sube la solución del problema expuesto en clase

--- 1ero/3er Trimestre/LND/generacion-xml/src/main/java/es/iesptodelacruz/lnd/GeneracionXml.java
+++ 1ero/3er Trimestre/LND/generacion-xml/src/main/java/es/iesptodelacruz/lnd/GeneracionXml.java
@@ -13,8 +13,38 @@ public final class GeneracionXml extends Utilidades{
      * @param args
      */
     public static void main(String[] args) {
+        
+        //Con este codigo logro realizar con exito lo pedido en el ejercicio
         Utilidades ut1=new Utilidades();
-        ut1.generarEtiquetasXml();
+        String generarEtiquetasXml = ut1.generarEtiquetasXml();
+        String generarEtiquetasRss = ut1.generarEtiquetasRss();
+        String generarEtiquetaTitle = ut1.generarEtiquetaTitle("Tarea 1");
+        String generarEtiquetaLink = ut1.generarEtiquetaLink("https://github.com/Gavila25");
+        String generarEtiquetaDescription = ut1.generarEtiquetaDescription("Esta es la descripcion de la etiqueta");
+        String contenidoChannel="\n\t\t"+generarEtiquetaTitle+"\n\t\t"+generarEtiquetaLink+"\n\t\t"+generarEtiquetaDescription;
+        String generarEtiquetaChannel = ut1.generarEtiqueta("channel", contenidoChannel, false);
+        System.out.println("Solucion 1, como se pedia en el ejercicio\n");
+        System.out.println(generarEtiquetasXml+"\n"
+        +generarEtiquetasRss+"\n\t"+
+        generarEtiquetaChannel+"\n\t</channel>"+"\n</rss>");
+
+
+        //Esto es por ver si me sale, no me lo tomes en cuenta si sale mal.
+        //Pretendo lograr poder crearlo con todos las etiquetas necesarios, eso incluye los items
+        String generarEtiquetasXml2 = ut1.generarEtiquetasXml();
+        String generarEtiquetasRss2 = ut1.generarEtiquetasRss();
+        String generarEtiquetaTitle2 = ut1.generarEtiquetaTitle("Tarea 2");
+        String generarEtiquetaLink2 = ut1.generarEtiquetaLink("https://github.com/Gavila25");
+        String generarEtiquetaDescription2 = ut1.generarEtiquetaDescription("Esta es la etiqueta nueva, que tambien llevara un item");
+        String generarcontenidoItem="\n\t\t\t"+generarEtiquetaTitle2+"\n\t\t\t"+generarEtiquetaLink2+"\n\t\t\t"+generarEtiquetaDescription2;
+        String generarEtiquetaItem = ut1.generarEtiqueta("item", generarcontenidoItem, false);
+        
+        System.out.println("\nSolución 2, agregando items al xml\n");
+        System.out.println("\n"+generarEtiquetasXml2+"\n"+
+        generarEtiquetasRss2+"\n\t"+
+        generarEtiquetaChannel+"\n\t\t"+generarEtiquetaItem+"\n\t\t</item>"
+        +"\n\t</channel"+"\n</rss>");
+        
 
     }
 }
--- 1ero/3er Trimestre/LND/generacion-xml/src/main/java/es/iesptodelacruz/lnd/Utilidades.java
+++ 1ero/3er Trimestre/LND/generacion-xml/src/main/java/es/iesptodelacruz/lnd/Utilidades.java
@@ -61,6 +61,6 @@ public class Utilidades {
     }
 
     
-
+    
 
 }
